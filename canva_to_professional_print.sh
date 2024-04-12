128dd4815a3042e7310ba1088b4c49d1eb233526
dachrisch <christian.daehn@gmail.com>
2024-04-12 14:13:16 +0200

    Update crop margins to improve print quality

--- canva_to_professional_print.sh
+++ canva_to_professional_print.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 # https://www.wir-machen-druck.de/tpl/manns-partner/media/ddb/druckdatenskizzen/0/brosch__re_mit_drahtheftung__endformat_quadrat_14_8_cm_x_14_8_cm__28-seitig_2.pdf
+
+# Exported PDF is
+# Paper size: 308 × 160 mm
+# Page size:  873.266 x 454.016 pts
+#
+# required output
+# Paper size print: 154 x 154 mm
+# Paper size: 436.633 x 436.633 pts
+# Content size print: 148 x 148 mm
+# Paper size: 420 x 420 pts
+
 infile="$1"
 outfile="$2"
 
@@ -9,12 +20,12 @@ right_pages=$(mktemp --suffix=.pdf)
 
 echo -n "extracting left pages..."
 pdfcrop --margins "0 0 -420 0" $infile $tempfile > /dev/null
-pdfcrop --margins "-18" $tempfile $left_pages > /dev/null
+pdfcrop --margins "-9" $tempfile $left_pages > /dev/null
 echo "done."
 
 echo -n "extracting right pages..."
 pdfcrop --margins "-420 0 0 0" $infile $tempfile > /dev/null
-pdfcrop --margins "-18" $tempfile $right_pages > /dev/null
+pdfcrop --margins "-9" $tempfile $right_pages > /dev/null
 echo "done."
 
 echo -n "assembling final booklet..."
